cad8d31dc6270a8be865dd92c5c5af5eff001318
jbeaken <jbeaken@elsevier.com>
2023-12-01 15:29:32 +0000

    adding test

--- src/test/java/org/mzuri/aoc2023/day1/Day1.java
+++ src/test/java/org/mzuri/aoc2023/day1/Day1.java
@@ -1,4 +1,22 @@
 package org.mzuri.aoc2023.day1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@Slf4j
 public class Day1 {
+
+
+    @Test
+    void test() throws IOException, URISyntaxException {
+        URI uri = Day1.class.getResource("day1Input.txt").toURI();
+        List<String> strings = Files.readAllLines(Path.of(uri));
+    }
 }
