74529dd990bb12425244c2af4de127aa6fdb2441
jbeaken <jbeaken@elsevier.com>
2023-12-01 17:01:24 +0000

    Test 2 with number words

--- src/test/java/org/mzuri/aoc2023/Day1.java
+++ src/test/java/org/mzuri/aoc2023/Day1.java
@@ -10,7 +10,9 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -18,36 +20,82 @@ public class Day1 {
 
 
     @Test
-    void test() throws IOException, URISyntaxException {
+    void test1() throws IOException, URISyntaxException {
         URI uri = Day1.class.getResource("../../../day1/input.txt").toURI();
         List<String> calibrations = Files.readAllLines(Path.of(uri));
 
         Integer total = 0;
-        for(String line : calibrations) {
-            Integer first = null;
-            Integer last = null;
+        for (String line : calibrations) {
+            Character first = null;
+            Character last = null;
 
             for (char c : line.toCharArray()) {
                 if (Character.isDigit(c)) {
-                    int numericValue = Character.getNumericValue(c);
                     if (first == null) {
-                        first = numericValue;
+                        first = c;
                     }
-                    last = numericValue;
+                    last = c;
                 }
             }
 
-            if(last == null) {
+            if (last == null) {
                 last = first;
             }
-            total += (first + last);
+            String toAdd = first.toString() + last.toString();
+            total += Integer.valueOf(toAdd);
 
-            log.info("line {} with first : {} and last : {} with total : {}", line, first, last, total);
-
+            log.info("line {} with toAdd : {}  with total : {}", line, toAdd, total);
 
 
         }
         log.info("Finished with {}", total);
+    }
 
+    @Test
+    void test2() throws IOException, URISyntaxException {
+
+        Map<String, Integer> numbers = Map.of("one", 1, "two", 2, "three", 3, "four", 4, "five", 5, "six", 6, "seven", 7, "eight", 8, "nine", 9);
+        URI uri = Day1.class.getResource("../../../day1/input.txt").toURI();
+        List<String> calibrations = Files.readAllLines(Path.of(uri));
+
+        int total = 0;
+
+        for (String line : calibrations) {
+
+            //pos, value
+            Map<Integer, Integer> numbersInLine = new HashMap<>();
+
+            //check for spelt out numbers
+            for (String number : numbers.keySet()) {
+                int pos = line.lastIndexOf(number);
+                if (pos != -1) {
+                    //got one
+                    numbersInLine.put(pos, numbers.get(number));
+                }
+            }
+
+            //check for digits
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                if (Character.isDigit(c)) {
+                    numbersInLine.put(i, Character.getNumericValue(c));
+                }
+            }
+
+            List<Integer> numbersFoundInOrder = numbersInLine.keySet().stream().sorted().toList();
+
+            String first = numbersInLine.get(numbersFoundInOrder.get(0)).toString();
+            Integer lastNumber = numbersInLine.get(numbersFoundInOrder.get(numbersFoundInOrder.size() - 1));
+            String last = lastNumber == null ? first : lastNumber.toString();
+
+
+            String toAdd = first + last;
+            total += Integer.parseInt(toAdd);
+
+            log.info("line {} with first : {}  last : {} toAdd : {} with total : {}", line, first, last, toAdd, total);
+
+
+        }
+        log.info("Finished with {}", total);
     }
 }
