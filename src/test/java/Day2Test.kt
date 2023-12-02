df802515e9038bdfd2bbf3ba983a5575e141a27f
jbeaken <jbeaken@elsevier.com>
2023-12-02 16:22:35 +0000

    getting kotlin good

--- src/test/java/Day2Test.kt
+++ src/test/java/Day2Test.kt
@@ -4,22 +4,54 @@ import kotlin.test.Test
 
 internal class Day2Test {
 
+    val idRegex = """Game (?<id>\d{1,2}):""".toRegex()
+    val redRegex = """(?<number>\d{1,2}) red""".toRegex()
+    val greenRegex = """(?<number>\d{1,2}) green""".toRegex()
+    val blueRegex = """(?<number>\d{1,2}) blue""".toRegex()
 
     @Test
-    fun testSum() {
+    fun testSum(): Int {
         val resource = this::class.java.getResource("input/day2.txt")
         val readAllLines = Files.readAllLines(Path.of(resource.toURI()))
 
-        readAllLines.forEach {
-            val regex = """\d""".toRegex()
+        readAllLines.take(1).forEach {line ->
+            println(line)
 
-            val matchResults = regex.findAll("abcb abbd")
 
-            println("The element is $it")
-            println("matchResults $matchResults")
-            matchResults.forEach {
-                println("The element is $it")
-            }
+
+            //Get id
+            val id = idRegex.find(line)?.groups?.get("id")?.value?.toInt()
+
+            val isPossible = getIsPossible(line)
+
+
         }
+    }
+
+    private fun getIsPossible(line: String): Boolean {
+        //red
+        var matchResults = redRegex.findAll(line)
+        val maxRedValue = matchResults.maxByOrNull{ it.groups["number"]?.value?.toInt() ?: 0 }?.groups?.get("number")?.value?.toInt()
+        println("Max red $maxRedValue")
+        if (maxRedValue != null) {
+            if(maxRedValue > 12) return false
+        }
+
+//green
+        matchResults = greenRegex.findAll(line)
+        val maxGreenValue = matchResults.maxByOrNull{ it.groups["number"]?.value?.toInt() ?: 0 }?.groups?.get("number")?.value?.toInt()
+        println("Max green $maxGreenValue")
+        if (maxGreenValue != null) {
+            if(maxGreenValue > 13) return false
+        }
+
+//blue
+        matchResults = blueRegex.findAll(line)
+        val maxBlueValue = matchResults.maxByOrNull{ it.groups["number"]?.value?.toInt() ?: 0 }?.groups?.get("number")?.value?.toInt()
+        println("Max blue $maxBlueValue")
+        if (maxBlueValue != null) {
+            if(maxBlueValue > 14) return false;
+        }
+
     }
 }
