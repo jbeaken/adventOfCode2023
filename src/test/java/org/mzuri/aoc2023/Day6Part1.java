c15e9ce4d4bca9734519be87a6ec90438e2b4ee1
jbeaken <jbeaken@elsevier.com>
2023-12-06 19:26:21 +0000

    setup again

--- src/test/java/org/mzuri/aoc2023/Day6Part1.java
+++ src/test/java/org/mzuri/aoc2023/Day6Part1.java
@@ -11,14 +11,22 @@ import java.util.Arrays;
 import java.util.List;
 
 @Slf4j(topic = "Day6")
+/**
+ * Time:      7  15   30
+ * Distance:  9  40  200
+ */
 public class Day6Part1 extends AdventOfCode2023Test {
 
     record AlmanacRange(Long destinationStart, Long sourceStart, Long range) {}
+
+    record Race(Integer duration, Integer record){}
 
     @Test
     void test() throws URISyntaxException, IOException {
         List<String> lines = loadInput("day6.txt");
         int result = 0;
+
+        lines.get(0).to
 
         Assertions.assertEquals(177942185, result);
     }
