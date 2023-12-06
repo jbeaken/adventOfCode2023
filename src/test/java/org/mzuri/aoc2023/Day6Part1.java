cfab3d0c12ef00769997774e2bfdb24598e8b0b4
jbeaken <jbeaken@elsevier.com>
2023-12-06 19:35:24 +0000

    looking good

--- src/test/java/org/mzuri/aoc2023/Day6Part1.java
+++ src/test/java/org/mzuri/aoc2023/Day6Part1.java
@@ -9,6 +9,8 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j(topic = "Day6")
 /**
@@ -17,17 +19,37 @@ import java.util.List;
  */
 public class Day6Part1 extends AdventOfCode2023Test {
 
-    record AlmanacRange(Long destinationStart, Long sourceStart, Long range) {}
-
-    record Race(Integer duration, Integer record){}
+    record Race(Integer time, Integer distance){}
 
     @Test
     void test() throws URISyntaxException, IOException {
         List<String> lines = loadInput("day6.txt");
-        int result = 0;
+        int result = 1;
 
-        lines.get(0).to
+        Pattern pattern = Pattern.compile("\\d{1,3}");
+        Matcher timeMatcher = pattern.matcher(lines.get(0));
+        Matcher distanceMatcher = pattern.matcher(lines.get(1));
 
-        Assertions.assertEquals(177942185, result);
+        List<Race> raceList = new ArrayList<>();
+        while(timeMatcher.find() && distanceMatcher.find()) {
+            Integer time = Integer.valueOf(timeMatcher.group());
+            Integer distance = Integer.valueOf(distanceMatcher.group());
+            Race race = new Race(time, distance);
+            raceList.add(race);
+        }
+
+        //cycle through races, getting number of ways to beat record
+        for (Race race : raceList) {
+            int noOfWaysToWin = 0;
+            for (int milisecondsPressed = 1; milisecondsPressed < race.time; milisecondsPressed++) {
+                if(milisecondsPressed * (race.time - milisecondsPressed) > race.distance) {
+                    //beat record
+                    noOfWaysToWin++;
+                }
+            }
+            result *= noOfWaysToWin;
+        }
+
+        Assertions.assertEquals(288, result);
     }
 }
